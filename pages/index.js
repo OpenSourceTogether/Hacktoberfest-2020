b60d05b23be502e2016e54adbfac63280b7e6afe
chandan-reddy-k <chandan@commutatus.com>
2020-10-07 10:37:24 +0530

    minor fixes

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ const Home = ({ contributors }) => (
       <FallingDownArrow />
     </Grid>
     <Grid container className={styles.contributorsListContainer}>
-      <Typography className={styles.contributorsTitle}>Open source contributors:</Typography>
+      <Typography className={styles.contributorsTitle}>{Math.floor(contributors.length/100)*100}+ contributors:</Typography>
       <Typography className={styles.contributorsSubTitle}>Tip: 👉 Click on an username to view their personalized music certificate.</Typography>
       {
         contributors && contributors.map((item, index) => {
